da0580f9e8b17f0467020535d41f538064c70362
faizacelebi <84246436+faizacelebi@users.noreply.github.com>
2022-04-07 13:10:42 -0700

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBdLpJ-QGSU5Rzus6_oskgAgDuhvEpF250",
+    authDomain: "kwitter-hw-aa152.firebaseapp.com",
+    projectId: "kwitter-hw-aa152",
+    storageBucket: "kwitter-hw-aa152.appspot.com",
+    messagingSenderId: "709775730787",
+    appId: "1:709775730787:web:5d95d80e777fadd02e3056",
+    measurementId: "G-SMGE4BPEEM"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);

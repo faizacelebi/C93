735555d690a6c5724078319ab7d2a22d6c21d6c7
faizacelebi <84246436+faizacelebi@users.noreply.github.com>
2022-04-12 21:46:29 -0700

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -18,7 +18,7 @@ const firebaseConfig = {
         room_name = document.getElementById("room_name").value;
         firebase.database().ref("/").child(room_name).update({purpose : "adding room name"});
         localStorage.setItem("room_name", room_name);
-        window.location = "kwitter_page.html";
+        window.location = "Kwitter_message_page.html";
 
   }
 
@@ -39,4 +39,10 @@ rediectToRoomName(){
     window.location = "kwitter_page.html";
 
 }
+
+function logout() {
+  localStorage.removeItem("user_name");
+  localStorage.removeItem("room_name");
+  window.location.replace("index.html");
+  }
 
